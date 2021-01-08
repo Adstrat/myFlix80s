4fe57f02608e27352d4dfd19edb55d683acd01f8
Adstrat <73385073+Adstrat@users.noreply.github.com>
2021-01-08 09:29:39 +0100

    uuid added

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express"),
-  morgan = require("morgan");
+  morgan = require("morgan"),
+  bodyParser = require("body-parser"),
+  uuid = require("uuid");
 
 const app = express();
 
 app.use(morgan("common"));
+app.use(bodyParser.json());
 
-let EightiesSciFi = [
+let eightiesSciFi = [
   {
     title: "Bladerunner",
     year: 1982,
@@ -68,16 +71,91 @@ let EightiesSciFi = [
   }
 ];
 
+//title page
 app.get("/", (req, res) => {
-  res.send("Welcome to the Ultimate 80s sci-fi app!");
+  res.send("Welcome to the Ultimate 80s sci-fi film app!");
 });
 
-app.get("/movies", (req, res) => {
-  res.json(EightiesSciFi);
+//opens documentation page
+app.use("/public", express.static("public"));
+
+//Return a list of ALL films
+app.get("/films", (req, res) => {
+  res.json(eightiesSciFi);
 });
 
-app.use("/index.html", express.static("public"));
+//Return information about ONE film
+app.get("/films/:title", (req, res) => {
+  res.json(
+    eightiesSciFi.find(film => {
+      return film.title === req.params.title;
+    })
+  );
+});
 
+//Return genre from title of film
+app.get("/films/genres/:title", (req, res) => {
+  res.send(
+    "Successful GET request returning data of genre for film: " +
+      req.params.title
+  );
+});
+
+//Return information about a director
+app.get("/films/directors/:name", (req, res) => {
+  res.send(
+    "Successful GET request returning data about director: " + req.params.name
+  );
+});
+
+//Allow users to register
+app.post("/users", (req, res) => {
+  let newUser = req.body;
+
+  if (!newUser.username) {
+    const message = "Missing name in request body";
+    res.status(400).send(message);
+  } else {
+    res.status(201).send(newUser);
+  }
+});
+
+//Allow users to update their information
+app.post("/users/:username", (req, res) => {
+  res.send(
+    "Successful POST request updating information for user:: " +
+      req.params.username
+  );
+});
+
+//Allow users to ADD a film to their favourites
+app.put("/users/:username/:film", (req, res) => {
+  res.send(
+    "Successful PUT request adding film '" +
+      req.params.film +
+      "' to user '" +
+      req.params.username +
+      "'"
+  );
+});
+
+//Allow users to REMOVE a film from their favourites
+app.delete("/users/:username/:film", (req, res) => {
+  res.send(
+    "Successful DELETE request deleting film '" +
+      req.params.film +
+      "' from user '" +
+      req.params.username +
+      "'"
+  );
+});
+
+//Allow users to deregister
+app.delete("/users/:username", (req, res) => {
+  res.send("Account deleted for " + req.params.username);
+});
+
+//ERROR MESSAGE
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Sorry, something went wrong!");
