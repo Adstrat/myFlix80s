f23ffdaa6721a3b6842f7de8019a85c93323d2ea
Adstrat <73385073+Adstrat@users.noreply.github.com>
2021-02-15 14:23:39 +0100

    passport required

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ app.use((err, req, res, next) => {
 let auth = require("./auth.js")(app);
 
 const passport = require("./passport.js");
+require("passport");
 
 //default text responce
 app.get("/", (req, res) => {
